8706d98f9b69c572de36f6402c2f73d5181ba10a
Numeral2 <126355981+Numeral2@users.noreply.github.com>
2025-04-01 19:11:09 +0200

    Update chatbot_rag.py

--- chatbot_rag.py
+++ chatbot_rag.py
@@ -4,7 +4,7 @@ import pdfplumber  # For extracting text from PDF
 from langchain_openai import OpenAIEmbeddings
 from langchain_pinecone import PineconeVectorStore
 from pinecone import Pinecone
-from langchain_community.chat_models import ChatOpenAI
+from langchain_openai import ChatOpenAI
 
 # Streamlit UI for user input (API keys and index name)
 st.title("Chatbot with PDF Upload")
